f9c4039befdf41a5ec8b4067df184425ce4fa503
narotamji <57707465+narotamji@users.noreply.github.com>
2020-09-22 11:02:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 500) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
+
+
+
+const switchColorButton = document.getElementById('switch-color__mode');
+const htmlTag = document.getElementById('HTML');
+const imgTags = document.querySelectorAll('img');
+
+function switchColorMode() {
+
+	htmlTag.classList.toggle('html-color');
+
+	for (const img of imgTags) {
+		img.classList.toggle('img-color');
+	}
+}
+
+switchColorButton.addEventListener('click', switchColorMode);
+
+// function lightMode() {
+
+// 	htmlTag.classList.add('html-color');
+
+// 	for (const img of imgTags) {
+// 		img.classList.add('img-color');
+// 	}
+
+// }
